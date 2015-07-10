4889da58b07c7090596b43eea7ec44c7fc1ddb4f
jmuraski <muraskijj@hotmail.com>
2015-07-10 14:00:46 -0500

    changed path for config file

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -3,7 +3,7 @@ var stripComments = require('strip-json-comments');
 var fs = require('fs');
 
 // you could do this all at once if you wanted to look cool
-var filename = resolve.sync('airbnb-style/linters/.eslintrc');
+var filename = resolve.sync('../../linters/.eslintrc');
 var data = fs.readFileSync(filename, {encoding: 'utf-8'});
 var dataWithoutComments = stripComments(data);
 var parsed = JSON.parse(dataWithoutComments);
